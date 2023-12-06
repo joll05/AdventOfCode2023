a8c403cb58067a3c0fe22953fd7e6c1d4ff2e27e
joll05 <joelolovnykanen@gmail.com>
2023-12-06 10:52:43 +0100

    Day 6

---
+++ Day 6/Solution2.py
@@ -0,0 +1,49 @@
+from math import sqrt, floor, ceil
+import re
+
+with open("input.txt") as f:
+    input_data = f.read().strip()
+
+"""
+Variable definitions:
+t = total race time
+c = charge time
+r = travelling time
+d = distance travelled
+
+The total time to race is the sum of charge and travelling time
+t = c + r <=> r = t - c
+
+The distance travelled is the travelling time times the charge time
+d = cr
+d = c(t - c)
+d = ct - c^2
+ct - c^2 - d = 0
+
+Multiply both sides by -1 and rearrange
+
+c^2 - ct + d = 0
+
+we can solve for c using the quadratic formula
+
+c = t/2 +- sqrt((t/2)^2 - d)
+c = t/2 +- sqrt(t^2/4 - d)
+
+c_min = t/2 - sqrt(t^2/4 - d)
+c_max = t/2 - sqrt(t^2/4 - d)
+
+If you plug in the total race time and the record distance, you get a minimum and maximimum charge time
+To make them into integer results, ceil min and floor max
+"""
+
+data_pattern = r"^Time: +([ \d]+)\nDistance: +([ \d]+)$"
+split_pattern = r" +"
+
+data_match = re.match(data_pattern, input_data)
+time = int("".join(re.split(split_pattern, data_match.group(1))))
+distance = int("".join(re.split(split_pattern, data_match.group(2))))
+
+root = sqrt(time ** 2 / 4 - distance)
+min_charge = ceil(time / 2 - root)
+max_charge = floor(time / 2 + root)
+print(max_charge - min_charge + 1)
