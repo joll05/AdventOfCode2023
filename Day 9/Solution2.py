e33e5117997e4af8ed9f5799294cb3e3a2bc895c
joll05 <joelolovnykanen@gmail.com>
2023-12-10 21:26:02 +0100

    Day 9

---
+++ Day 9/Solution2.py
@@ -0,0 +1,21 @@
+with open("input.txt") as f:
+    input_lines = f.read().strip().split("\n")
+
+inputs = tuple(tuple(map(int, line.split(" "))) for line in input_lines)
+
+result = 0
+
+for values in inputs:
+    diffs = [values]
+
+    while any(diffs[-1]): # While all is not 0 (false)
+        diffs.append(tuple(diffs[-1][i + 1] - diffs[-1][i] for i in range(len(diffs[-1]) - 1)))
+    
+    addition = 0
+
+    for diff in diffs[::-1][1:]:
+        addition = diff[0] - addition
+    
+    result += addition
+
+print(result)
